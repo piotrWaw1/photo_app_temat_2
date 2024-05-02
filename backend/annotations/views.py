60ea8ebece679c65cefe287ed95bef693e8c26e1
wojwoj8 <wojwoj8@gmail.com>
2024-05-02 13:01:03 +0200

    setup for sending images

--- backend/annotations/views.py
+++ backend/annotations/views.py
@@ -3,11 +3,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Photo
 from .serializer import PhotoSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class PhotoCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request, *args, **kwargs):
+        print(request)
         serializer = PhotoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(owner=request.user)  # Assuming user is authenticated
+            serializer.save(owner=request.user)  
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
--- frontend/src/dashboard/Annotation.tsx
+++ frontend/src/dashboard/Annotation.tsx
@@ -1,22 +1,54 @@
 import React from 'react';
 import axios from 'axios';
+import { useState, useContext } from 'react';
+import { useSessionContext } from '../hooks/useSessionContext';
+
 
 const Annotation: React.FC = () => {
-  const sendRequest = async () => {
-    try {
-      const response = await axios.post('/annotations/photos', {
-        // dane, jeśli potrzebne
-      });
 
-      console.log(response.data);
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+  const {tokens} = useSessionContext()
+  console.log(tokens?.access);
+  // for posting images
+  const [selectedImageFile, setSelectedImageFile] = useState<File | null>(null);
+  const allowedFormats = ['image/jpeg','image/jfif', 'image/jpg', 'image/png', 'image/bmp', 'image/gif'];
+
+  const handleImageUpload = async (e) => {
+    e.preventDefault();
+    if (e.target){
+      // console.log(e.target)
+      const formData = new FormData(e.target)
+      
+      try {
+        const response = await axios.post('/annotations/photos', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: 'Bearer ' + String(tokens.access),
+            },
+        });
+  
+        console.log(response.data);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
     }
+    
   };
 
   return (
     <div>
-      <button onClick={sendRequest}>Send Request</button>
+      <form onSubmit={handleImageUpload}>
+        <label htmlFor="uploadImage">upload image</label>
+        <input 
+          className=''
+          id='uploadImage'
+          type='file'
+          accept=".jpeg, .jpg, .png, .jfif, .bmp, .gif"
+        />
+        <input type='submit' value={"submit form"}/>
+      </form>
+
+
+      {/* <button onClick={sendRequest}>Send Request</button> */}
     </div>
   );
 };
